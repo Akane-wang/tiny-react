83a299ec56d8db1dd946d910d8b164c18bf3cdb5
wangqian <wangqian@sz-bcs.com.cn>
2021-12-17 15:03:51 +0800

    feat(basic-component): 修改引用方式

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
-import PureTabBar,  { IProps as IPureTabBarProps, ITabBar } from '@/basic/PureTabBar';
-import ReoIcon, { EIconType, IProps as IIconProps } from '@/basic/ReoIcon';
-import ReoButton, { ReoCloseButton, ReoCarouselButton } from '@/basic/ReoButton';
-import ReoBadge, { IProps as ReoBadgeProps } from '@/basic/ReoBadge';
-import ReoInput, { Size, CurrentState, Types, TipState, IProps as IInputProps } from '@/basic/ReoInput';
-import ReoLink, { IProps as ILinkProps } from '@/basic/ReoLink';
-import ReoPagination, { IProps as IPaginationProps } from '@/basic/ReoPagination';
-import ReoPopup, { IProps as IPopupProps } from '@/basic/ReoPopup';
-import { ReoBlueIconSearch, IProps as ISearchProps, ReoSearch } from '@/basic/ReoSearch';
-import ReoSelect, {
-    IProps as ISelectProps, IInnerOption, IOptionsList, TOptionKey, IDropDown,  IOption
-} from '@/basic/ReoSelect';
+export { default as PureTabBar } from '@/basic/PureTabBar';
+export type { IProps as IPureTabBarProps, ITabBar } from '@/basic/PureTabBar';
+export { default as ReoIcon } from '@/basic/ReoIcon';
+export type { EIconType, IProps as IIconProps } from '@/basic/ReoIcon';
+export { default as ReoButton } from '@/basic/ReoButton';
+export type { ReoCloseButton, ReoCarouselButton } from '@/basic/ReoButton';
+export { default as ReoBadge } from '@/basic/ReoBadge';
+export type { IProps as ReoBadgeProps } from '@/basic/ReoBadge';
+export { default as ReoInput } from '@/basic/ReoInput';
+export type { Size, CurrentState, Types, TipState, IProps as IInputProps } from '@/basic/ReoInput';
+export { default as ReoLink } from '@/basic/ReoLink';
+export type { IProps as ILinkProps } from '@/basic/ReoLink';
+export { default as ReoPagination } from '@/basic/ReoPagination';
+export type { IProps as IPaginationProps } from '@/basic/ReoPagination';
+export { default as ReoPopup } from '@/basic/ReoPopup';
+export type { IProps as IPopupProps } from '@/basic/ReoPopup';
+export { ReoBlueIconSearch } from '@/basic/ReoSearch';
+export type { IProps as ISearchProps, ReoSearch } from '@/basic/ReoSearch';
+// import ReoSelect, {
+//     IProps as ISelectProps, IInnerOption, IOptionsList, TOptionKey, IDropDown,  IOption
+// } from '@/basic/ReoSelect';
+
+export { default as ReoSelect } from '@/basic/ReoSelect';
+export type { IProps as ISelectProps, IInnerOption, IOptionsList, TOptionKey, IDropDown,  IOption } from '@/basic/ReoSelect';
 export {
-    ReoButton, ReoCloseButton, ReoCarouselButton, // button
-    EIconType, IIconProps, ReoIcon, // icon
-    PureTabBar, IPureTabBarProps, ITabBar, // tabBar
-    ReoBadge, ReoBadgeProps, // badge
-    Size, CurrentState, Types, TipState, IInputProps, ReoInput, // input
-    ReoLink, ILinkProps, // link
-    ReoPagination, IPaginationProps, // pagination
-    ReoPopup, IPopupProps, // popup
-    ReoBlueIconSearch, ISearchProps, ReoSearch, // search, blueIconSearch
-    ReoSelect, ISelectProps, IInnerOption, IOptionsList, TOptionKey, IDropDown,  IOption, // select
+    // ReoButton, ReoCloseButton, ReoCarouselButton, // button
+    // EIconType, IIconProps, ReoIcon, // icon
+    // PureTabBar, IPureTabBarProps, ITabBar, // tabBar
+    // ReoBadge, ReoBadgeProps, // badge
+    // Size, CurrentState, Types, TipState, IInputProps, ReoInput, // input
+    // ReoLink, ILinkProps, // link
+    // ReoPagination, IPaginationProps, // pagination
+    // ReoPopup, IPopupProps, // popup
+    // ReoBlueIconSearch, ISearchProps, ReoSearch, // search, blueIconSearch
+    // ReoSelect, ISelectProps, IInnerOption, IOptionsList, TOptionKey, IDropDown,  IOption, // select
 };
